c102f69c83cc022fc6221970accffc2f82271d32
paul <pbranz@gmail.com>
2022-03-08 11:30:18 +0000

    commit

--- credentials.h
+++ credentials.h
@@ -1,7 +1,7 @@
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
-#define WIFI_SSID_CREDENTIALS "MyWiFiSSID"
+#define WIFI_SSID_CREDENTIALS "MyWiFiSSIDTEST"
 #define WIFI_PW_CREDENTIALS "some_secret_password"
 #define MQTT_USER_CREDENTIAL "MyUser"
 #define MQTT_PW_CREDENTIAL "another_secret_password"
